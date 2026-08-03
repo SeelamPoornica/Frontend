fcb920c53f571c880aebcad13b0ad6f3cfcccc87
SEELAM POORNICA <seelampoornica@gmail.com>
2026-08-03 06:45:56 +0530

    Create products.js

---
+++ day-13/products.js
@@ -0,0 +1,62 @@
+// product.js
+// This file just stores all our product data in one array.
+// Each product is an object with: id, name, description, price, image
+
+let products = [
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    description: "Comfortable over-ear headphones with good bass.",
+    price: 1499,
+    image: "https://shotkit.com/wp-content/uploads/2025/05/shotkit-sonos-ace-5-768x512.jpg"
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    description: "Tracks steps, heart rate and shows notifications.",
+    price: 2999,
+    image: "https://tse1.mm.bing.net/th/id/OIP.Bi6H2bGIVTjoKBmc6_AVFwHaHa?r=0&pid=Api&h=220&P=0"
+  },
+  {
+    id: 3,
+    name: "Running Shoes",
+    description: "Lightweight shoes, great for daily running.",
+    price: 1999,
+    image: "https://tse4.mm.bing.net/th/id/OIP.K7FZsO6IklcF--v19jz_XAHaE8?r=0&pid=Api&h=220&P=0"
+  },
+  {
+    id: 4,
+    name: "Backpack",
+    description: "Spacious laptop backpack with many pockets.",
+    price: 1299,
+    image: "https://tse4.mm.bing.net/th/id/OIP.3ylJKR4a5i6ZXr2ULssj4QHaKu?r=0&pid=Api&h=220&P=0"
+  },
+  {
+    id: 5,
+    name: "Bluetooth Speaker",
+    description: "Portable speaker with clear sound and long battery.",
+    price: 1799,
+    image: "https://tse1.mm.bing.net/th/id/OIP.g-IXaRH2deMRDkasq9BDFQHaHa?r=0&pid=Api&h=220&P=0"
+  },
+  {
+    id: 6,
+    name: "Coffee Mug",
+    description: "Ceramic mug, keeps your coffee warm longer.",
+    price: 299,
+    image: "https://tse1.mm.bing.net/th/id/OIP.Q2botIajRfwXp9jHAZzxPQHaHa?r=0&pid=Api&h=220&P=0"
+  },
+  {
+    id: 7,
+    name: "Sunglasses",
+    description: "UV protected sunglasses with stylish frame.",
+    price: 899,
+    image: "https://tse4.mm.bing.net/th/id/OIP.CNo-kShfDHnnCDzH8Bt-BwHaDt?r=0&pid=Api&h=220&P=0"
+  },
+  {
+    id: 8,
+    name: "Table Lamp",
+    description: "Adjustable LED lamp for study or work desk.",
+    price: 999,
+    image: "https://m.media-amazon.com/images/I/819ffYEkCfL.jpg"
+  }
+];
